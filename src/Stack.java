629c6b6ed5f99f98d2826c925f317241de7387d6
man-r <man-r@1W07-13.uwds.uwinnipeg.ca>
2018-01-16 11:19:28 -0600

    Created stack object, haven't tested yet.

--- src/Stack.java
+++ src/Stack.java
@@ -21,30 +21,35 @@ public class Stack{//begin class
  	
     //**********instance variable**********
     
-        ArrayList stack = new ArrayList();
- 	
+        public ArrayList stack;
+    
     //**********constructors***********
         
         public Stack(){
             System.out.println("New stack being created");
+            stack = new ArrayList();
         }//end constructor
  	
     //**********observers**********
         
-        public static Object peek(ArrayList s){
-            return s.get(0);
+        public int peek(){
+            return (int) stack.get(0);
         }//end get top value
+        
+        public int look(){
+            return (int) stack.size();
+        }//for testing only
  	
     //**********transformers**********
         
-        public static Object pop(ArrayList s){
-            Object temp = s.get(0);
-            s.remove(0);
+        public int pop(){
+            int temp = (int) stack.get(0);
+            stack.remove(0);
             return temp;
         }//end remove top value
         
-        public static void push(ArrayList s, int n){
-            s.add(0, n);
+        public void push(int n){
+            stack.add(0, n);
         }//end push value on top of stack
  
 }//end class
